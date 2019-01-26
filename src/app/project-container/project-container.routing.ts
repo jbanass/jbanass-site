6390ffcffac341cf01548917af3efde9ad71e23b
Joseph Banass <jbanass@gmail.com>
2019-01-26 12:01:35 -0600

    Fixing default route path match

--- src/app/project-container/project-container.routing.ts
+++ src/app/project-container/project-container.routing.ts
@@ -1,11 +1,11 @@
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule } from "@angular/router";
 
-import { ProjectComponent } from '../project/project.component';
-import {EmptyComponent} from '../empty/empty.component';
+import { ProjectComponent } from "../project/project.component";
+import { EmptyComponent } from "../empty/empty.component";
 
 const appRoutes: Routes = [
-    { path: '', component: EmptyComponent },
-    { path: 'p/:name', component: ProjectComponent }
+  { path: "", redirectTo: "/", pathMatch: "full" },
+  { path: "p/:name", component: ProjectComponent }
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
