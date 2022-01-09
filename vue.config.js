d65b9533f1a9126c3beb4db0310347294150ee94
Joseph Banass <jbanass@benchprep.com>
2022-01-09 16:39:50 -0600

    Update Vue config to make variables available
    Instead of importing the variables scss file in every component
    that needs to declare styles and also use variables, we update the
    Vue config to make the variables globally available in any component.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   lintOnSave: false,
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: '@import "@/assets/styles/variables.scss";',
+      },
+    },
+  },
 };
