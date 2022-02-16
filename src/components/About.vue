740603842722cbb81c7a80b681d10e797cd5cd8d
Joseph Banass <jbanass@gmail.com>
2022-02-15 20:47:32 -0600

    Initial commit of mobile layout

---
+++ src/components/About.vue
@@ -0,0 +1,47 @@
+<template>
+  <section>
+    <div class="about">
+      <div class="about-img">
+        <img
+          title="I prefer Spaces over Tabs"
+          alt="A floating astronaut in space on a laptop programming"
+          src="../assets/Astronaut.png"
+        />
+      </div>
+      <div>
+        <h2 class="section-header">Hello</h2>
+        <div class="section-subheader">Passionate. Involved. Dedicated.</div>
+        <p class="section-description">
+          I am an extremely motivated Chicago-based developer immersed in the
+          potential of computers since the ripe age of seven. Always consumed by
+          the latest hardware and software trends, I live and breathe to push
+          the limits of what people think apps can do.
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "About",
+};
+</script>
+
+<style lang="scss" scoped>
+.about {
+  display: flex;
+  flex-direction: column;
+  margin: 60px 40px;
+}
+
+.about-img {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 60%;
+  }
+}
+</style>
---
+++ tests/unit/components/about.spec.js
@@ -0,0 +1,56 @@
+import { shallowMount } from "@vue/test-utils";
+import About from "@/components/About.vue";
+
+describe("About", () => {
+  describe("astronaut image", () => {
+    it("renders the image", () => {
+      const wrapper = shallowMount(About);
+      expect(wrapper.find(".about-img img").exists()).toBe(true);
+    });
+
+    it("includes alt text", () => {
+      const wrapper = shallowMount(About);
+      expect(wrapper.find(".about-img img").element.alt).toEqual(
+        "A floating astronaut in space on a laptop programming"
+      );
+    });
+
+    it("includes title", () => {
+      const wrapper = shallowMount(About);
+      expect(wrapper.find(".about-img img").element.title).toEqual(
+        "I prefer Spaces over Tabs"
+      );
+    });
+  });
+
+  describe("content", () => {
+    describe("header", () => {
+      it("has a text content stating 'HELLO'", () => {
+        const wrapper = shallowMount(About);
+        expect(
+          wrapper.find(".section-header").element.textContent.trim()
+        ).toEqual("Hello");
+      });
+    });
+
+    describe("subheader", () => {
+      it("has a text content stating 'Passionate. Involved. Dedicated.'", () => {
+        const wrapper = shallowMount(About);
+        expect(
+          wrapper.find(".section-subheader").element.textContent.trim()
+        ).toEqual("Passionate. Involved. Dedicated.");
+      });
+    });
+
+    describe("description", () => {
+      it("includes the proper content for the description", () => {
+        const wrapper = shallowMount(About);
+        expect(
+          wrapper.find(".section-description").element.textContent.trim()
+        ).toEqual(
+          "I am an extremely motivated Chicago-based developer immersed in the potential of computers since the ripe age of seven. Always consumed by the latest hardware and software trends, I live and breathe to push the limits of what people think apps can do."
+        );
+      });
+    });
+  });
+});
